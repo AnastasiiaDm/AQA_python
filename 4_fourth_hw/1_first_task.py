932271da7118e71569387f7924d12f04cc7b241a
Anastasiia <nasik@Anastasiias-MacBook-Pro.local>
2023-01-25 03:10:37 +0200

    add fourth task

---
+++ 4_fourth_hw/1_first_task.py
@@ -0,0 +1,8 @@
+"""
+there is a space-separated string with the names "john peter brian Morgan Adam Maria bart" convert the string so that
+each name starts with a capital letter.
+"""
+
+my_str = "john peter brian Morgan Adam Maria bart"
+
+print(my_str.title())
