12c4eebb2969532dc67d18a1dce2b772474f9dcd
Anastasiia <nasik@Anastasiias-MacBook-Pro.local>
2023-01-29 20:17:43 +0200

    add another way of solution

--- 4_fourth_hw/4_fouth_task.py
+++ 4_fourth_hw/4_fouth_task.py
@@ -2,6 +2,7 @@
 you have a list of variable names in camel case format ["FirstItem", "FriendsList", "MyTuple"] convert it to a list of
 variable names for python in snake case format ["first_item", "friends_list", "my_tuple"]
 """
+import re
 
 variable_list = ["FirstItem", "FriendsList", "MyTuple"]
 new_list = []
@@ -15,3 +16,10 @@ if __name__ == '__main__':
         stripped = r.lstrip("_")
         new_list.append(stripped)
     print(new_list)
+
+# anoter way of solution:
+if __name__ == '__main__':
+    pattern = re.compile(r'([A-Z][a-z]+)([A-Z][a-z]+)')
+    matches = pattern.sub(r'\1_\2', ','.join(variable_list)).lower()
+    result = matches.split(',')
+    print(result)
